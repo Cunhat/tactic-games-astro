5821895f21944856d2b6e41e1cc399cfe7dd1976
Tiago Cunha <tiago92cunha@gmail.com>
2024-01-13 14:39:03 +0000

    Fix menu on mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,12 +11,12 @@ export const Navbar = () => {
   };
 
   return (
-    <nav className="p-4 top-0 z-50 w-full bg-black sm:gap-24 justify-center flex">
+    <nav className="p-3 top-0 z-50 w-full bg-black sm:gap-24 justify-center flex">
       <div className="container flex w-full justify-between sm:justify-center items-center">
         <img src="/img/logo.png" alt="logo-img" width="60" height="60" />
         <Icons.menu className="h-8 w-8 sm:hidden" onClick={handleClick} />
         {show && (
-          <div className="pt-[20%] p-4 absolute top-0 left-0 right-0 bottom-0 bg-black h-screen w-screen flex flex-col gap-[10%] items-center">
+          <div className="pt-[20%] p-4 absolute z-50 top-0 left-0 right-0 bottom-0 bg-black h-screen w-screen flex flex-col gap-[10%] items-center">
             <img
               src="/img/logo.png"
               width={"70%"}
