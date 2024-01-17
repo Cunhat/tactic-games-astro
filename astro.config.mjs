8282634d8c9d0f34dfd227823eb22dd5cfce25d3
Tiago Cunha <tiago92cunha@gmail.com>
2024-01-17 22:16:21 +0000

    Add markdown styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
+import remarkCodeTitles from 'remark-code-titles';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
+
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkCodeTitles],
+    extendDefaultPlugins: true,
+  },
   integrations: [tailwind(), react(), mdx()]
 });
